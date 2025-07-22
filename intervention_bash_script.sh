e28b6d1ab399a5ee34858cab7bb60021d863f00b
TongmengXie <ucfntxi@ucl.ac.uk>
2025-07-22 21:32:19 +0000

    added choice of device

---
+++ intervention_bash_script.sh
@@ -0,0 +1,9 @@
+# unsteered
+python features_intervention.py --model_name Llama-3.2-1B --extracting_from mmlu-pro_600 --device "cuda:1"> ../../outputs/Llama-3.2-1B_test_on_MMLU600.log
+
+python features_intervention.py --model_name Llama-3.2-1B-Instruct --extracting_from  mmlu-pro_600 --device "cuda:1"> ../../outputs/Llama-3.2-1B-Instruct_test_on_MMLU600.log 
+
+python features_intervention.py --model_name llama_3.2_1b_instruct_rlhf --extracting_from  mmlu-pro_600 --device "cuda:1" > ../../outputs/llama_3.2_1b_instruct_rlhf_test_on_MMLU600.log 
+
+# steered
+python features_intervention.py --model_name Llama-3.2-1B --extracting_from mmlu-pro_600 --device "cuda:1" --Intervention True > ../../outputs/Llama-3.2-1B_test_on_MMLU600.log
