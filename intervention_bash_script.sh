4bbaa2cdcb0904ad08c1763775fcc05c4d87eda3
TongmengXie <ucfntxi@ucl.ac.uk>
2025-07-24 17:18:31 +0000

    chores

--- intervention_bash_script.sh
+++ intervention_bash_script.sh
@@ -11,5 +11,7 @@ python features_intervention.py --model_name Llama-3.2-1B --extracting_from mmlu
 python features_intervention.py --model_name Llama-3.2-1B-Instruct --extracting_from mmlu-pro_600 --device "cuda:1" --Intervention True > ../../outputs/intervened_Llama-3.2-1B-Instruct_test_on_MMLU600.log
 
 
-# CoT unsteered
-python features_intervention.py --model_name DeepSeek-R1-Distill-Qwen-1.5B --extracting_from mmlu-pro_600 --device "cuda:1"> ../../outputs/DeepSeek-R1-Distill-Qwen-1.5B_test_on_MMLU600.log
+# CoT
+python features_intervention.py --model_name Llama-3.2-1B-Instruct --extracting_from mmlu-pro_600 --device "cuda:2" --Intervention True > ../../outputs/Llama-3.2-1B-Instruct_test_on_MMLU600.log
+
+python features_intervention.py --model_name DeepSeek-R1-Distill-Qwen-1.5B --extracting_from mmlu-pro_600 --device "cuda:1" --Intervention True > ../../outputs/DeepSeek-R1-Distill-Qwen-1.5B_test_on_MMLU600.log
